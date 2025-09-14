69237cb98dc1a801e7440c7211d464df9716f69e
igorb <igorbabin2002@gmail.com>
2025-09-14 23:54:30 +0500

    Added roles to GetMe response

--- coreapi/src/Features/Users/Actions/GetMe.cs
+++ coreapi/src/Features/Users/Actions/GetMe.cs
@@ -9,17 +9,29 @@ public static partial class GetMe
 {
     public record Request;
 
+    public record Response : SeadoxUser.Model
+    {
+        public required IReadOnlyCollection<string> Roles { get; set; }
+    } 
+
     internal static void CustomizeEndpoint(IEndpointConventionBuilder endpoint) => endpoint
         .RequireAuthorization()
         .WithTags(nameof(SeadoxUser))
         .WithDescription("Получение данных специфичных для Seadox Api");
 
-    private static async ValueTask<Results<UnauthorizedHttpResult, Ok<SeadoxUser.Model>>> HandleAsync(
+    private static async ValueTask<Results<UnauthorizedHttpResult, Ok<Response>>> HandleAsync(
         Request _,
         CallerContext caller,
         CancellationToken ct)
     {
         var state = await caller.GetRequiredStateAsync(ct);
-        return TypedResults.Ok(state.User);
+        return TypedResults.Ok(new Response
+        {
+            Id = state.User.Id,
+            ZitadelId = state.User.ZitadelId,
+            AvatarUrl = state.User.AvatarUrl,
+            Color = state.User.Color,
+            Roles = state.Roles
+        });
     }
 }
